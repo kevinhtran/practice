8218586dc5900d2f27dc72eb1aadffc8a4aa639d
kevinhtran <kevdeveloper7@gmail.com>
2021-09-15 17:16:13 -0700

    Using immediately-invoked async await expressions

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Exploring Fetch
-
-// const result = fetch('https://pokeapi.co/api/v2/pokemon/charmander')
-//   .then(response => response.json())
-//   .then(result => console.log(result))
-//   .catch(error => console.error(error));
+const result = fetch('https://pokeapi.co/api/v2/pokemon/charmander')
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.error(error));
 
 // Converting to Async Await
 // Which is the more preferred way
@@ -13,3 +12,23 @@ const getPokemon = async () => {
   console.log(data)
 }
 getPokemon();
+
+// Immediately-Invoked Function Expression (IIFE)
+
+(async function () {
+  const result = await fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+  const data = await result.json()
+  console.log(data)
+})()
+
+  // Immediately-Invoked Asynchronous Arrow Functions
+  // it's wrapped in parenthesis because we give it it's own scope to execute as an espression to result to a value
+
+  (async () => {
+    // fetch returns to us a promise
+    // await - doesn't go to the next line until this has resolved without the await keyword
+    const result = await fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+    // it immediately goes to the next line 
+    const data = await result.json()
+    console.log(data)
+  })()
