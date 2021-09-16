a80dbf3c71c0cf191dc574cdf65682db3998d565
kevinhtran <kevdeveloper7@gmail.com>
2021-09-15 17:06:07 -0700

    using async await instead of fetch

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Exploring Fetch
 
-const result = fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-  .then(response => response.json())
-  .then(result => console.log(result))
-  .catch(error => console.error(error));
+// const result = fetch('https://pokeapi.co/api/v2/pokemon/charmander')
+//   .then(response => response.json())
+//   .then(result => console.log(result))
+//   .catch(error => console.error(error));
+
+// Converting to Async Await
+// Which is the more preferred way
+const getPokemon = async () => {
+  const result = await fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+  const data = await result.json()
+  console.log(data)
+}
+getPokemon();
