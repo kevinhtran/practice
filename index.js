37ce1c39f1fc55e18860d51f540b2b1edc639221
kevinhtran <kevdeveloper7@gmail.com>
2021-09-21 12:08:17 -0700

    interactions: alert, prompt, confirm

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-// let isGreater = typeof NaN
-// console.log(isGreater);
+// result = prompt('enter your name', 'name')
+// console.log(result)
 
-// const obj0 = 1 = {}
-// const obj1 = 2 = // null value. pointing to no object
+// let isBoss = confirm("Are you the boss?")
+// console.log(isBoss)
 
-// // undefined
-// let value
-// console.log(value)
-
-// console.log(Math.ceil(4.1528390482))
-
-// //typeof
-// const alert = () => {
-//   // console.log("hello")
-//   return "hello"
-// }
-
-// console.log(typeof alert())
-// console.log(typeof Symbol())
-
-// let name = "Ilya";
-
-// console.log(`hello ${1}`); // hello 1
-
-// console.log(`hello ${"name"}`); // hello name
-
-// console.log(`hello ${name}`); // hello Ilya\
-
+let askName = prompt("Enter your name", "Your name");
+alert(askName);
